cf528f72c5db045072ada944b135d5a629467e93
Burew <notinschool@gmail.com>
2017-05-01 20:57:26 -0700

    Added Board Members, need to fix images

--- js/creative.js
+++ js/creative.js
@@ -4,9 +4,41 @@ $(".hover").mouseleave(
     function () {
         $(this).removeClass("hover");
     }
-);
-
+); 
 //ics day snippet close
+
+
+//Board member snippet
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");	//description
+  var dots = document.getElementsByClassName("demo");  	//icon image
+  if (n > x.length) {slideIndex = 1}
+	  if (n < 1) {slideIndex = x.length}
+		  for (i = 0; i < x.length; i++) {
+			 x[i].style.display = "none";
+		  }
+  for (i = 0; i < dots.length; i++) {
+     dots[i].className = dots[i].className.replace(" board-members-opacity-off", "");
+	 dots[i].style.border = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " board-members-opacity-off";
+  dots[slideIndex-1].style.border = "3px solid red";
+}
+
+//end Board member snippet
 
 
 
