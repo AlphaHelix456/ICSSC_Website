3903856216230e56252987a4c6666d27ca73b50b
cleider <cleidergo@gmail.com>
2017-07-17 20:20:36 -0700

    fixed the bottom form

--- js/creative.js
+++ js/creative.js
@@ -1,112 +1,113 @@
 //ics day snipet
 
+    $(".hover").mouseleave(
+        function () {
+            $(this).removeClass("hover");
+        }
+    );
+    //ics day snippet close
 
 
-$(".hover").mouseleave(
-    function () {
-        $(this).removeClass("hover");
+    //Board member snippet
+    var slideIndex = 1;
+    showDivs(slideIndex);
+
+    function plusDivs(n) {
+      showDivs(slideIndex += n);
     }
-); 
-//ics day snippet close
 
+    function currentDiv(n) {
+      showDivs(slideIndex = n);
+    }
 
-//Board member snippet
-var slideIndex = 1;
-showDivs(slideIndex);
+    function showDivs(n) {
+      var i;
+      var x = document.getElementsByClassName("mySlides");	//description
+      var dots = document.getElementsByClassName("demo");  	//icon image
+      if (n > x.length) {slideIndex = 1}
+          if (n < 1) {slideIndex = x.length}
+              for (i = 0; i < x.length; i++) {
+                 x[i].style.display = "none";
+              }
+      for (i = 0; i < dots.length; i++) {
+         dots[i].className = dots[i].className.replace(" board-members-opacity-off", "");
+         dots[i].style.border = "none";
+      }
+      x[slideIndex-1].style.display = "block";
+      dots[slideIndex-1].className += " board-members-opacity-off";
+      dots[slideIndex-1].style.border = "3px solid red";
+    }
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-
-function currentDiv(n) {
-  showDivs(slideIndex = n);
-}
-
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");	//description
-  var dots = document.getElementsByClassName("demo");  	//icon image
-  if (n > x.length) {slideIndex = 1}
-	  if (n < 1) {slideIndex = x.length}
-		  for (i = 0; i < x.length; i++) {
-			 x[i].style.display = "none";
-		  }
-  for (i = 0; i < dots.length; i++) {
-     dots[i].className = dots[i].className.replace(" board-members-opacity-off", "");
-	 dots[i].style.border = "none";
-  }
-  x[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " board-members-opacity-off";
-  dots[slideIndex-1].style.border = "3px solid red";
-}
-
-//end Board member snippet
+    //end Board member snippet
 
 
 
-(function($) {
-    "use strict"; // Start of use strict
+    (function($) {
+        "use strict"; // Start of use strict
 
-    // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $(document).on('click', 'a.page-scroll', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
-        }, 1250, 'easeInOutExpo');
-        event.preventDefault();
-    });
+        // jQuery for page scrolling feature - requires jQuery Easing plugin
+        $(document).on('click', 'a.page-scroll', function(event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: ($($anchor.attr('href')).offset().top - 50)
+            }, 1250, 'easeInOutExpo');
+            event.preventDefault();
+        });
 
-    // Highlight the top nav as scrolling occurs
-    $('body').scrollspy({
-        target: '.navbar-fixed-top',
-        offset: 51
-    });
+        // Highlight the top nav as scrolling occurs
+        $('body').scrollspy({
+            target: '.navbar-fixed-top',
+            offset: 51
+        });
 
-    // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a').click(function() {
-        $('.navbar-toggle:visible').click();
-    });
+        // Closes the Responsive Menu on Menu Item Click
+        $('.navbar-collapse ul li a').click(function() {
+            $('.navbar-toggle:visible').click();
+        });
 
-    // Offset for Main Navigation
-    $('#mainNav').affix({
-        offset: {
-            top: 100
-        }
-    })
+        // Offset for Main Navigation
+        $('#mainNav').affix({
+            offset: {
+                top: 100
+            }
+        })
 
-    // Initialize and Configure Scroll Reveal Animation
-    window.sr = ScrollReveal();
-    sr.reveal('.sr-icons', {
-        duration: 600,
-        scale: 0.3,
-        distance: '0px'
-    }, 200);
-    sr.reveal('.sr-button', {
-        duration: 1000,
-        delay: 200
-    });
-    sr.reveal('.sr-contact', {
-        duration: 600,
-        scale: 0.3,
-        distance: '0px'
-    }, 300);
+        // Initialize and Configure Scroll Reveal Animation
+        window.sr = ScrollReveal();
+        sr.reveal('.sr-icons', {
+            duration: 600,
+            scale: 0.3,
+            distance: '0px'
+        }, 200);
+        sr.reveal('.sr-button', {
+            duration: 1000,
+            delay: 200
+        });
+        sr.reveal('.sr-contact', {
+            duration: 600,
+            scale: 0.3,
+            distance: '0px'
+        }, 300);
 
-    // Initialize and Configure Magnific Popup Lightbox Plugin
-    $('.popup-gallery').magnificPopup({
-        delegate: 'a',
-        type: 'image',
-        tLoading: 'Loading image #%curr%...',
-        mainClass: 'mfp-img-mobile',
-        gallery: {
-            enabled: true,
-            navigateByImgClick: true,
-            preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
-        },
-        image: {
-            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-        }
-    });
-	
-	//$("div.item p,h1").css({"font-size":"1.5em", "font-weight":"bold", "color":"#000"});
+        // Initialize and Configure Magnific Popup Lightbox Plugin
+        $('.popup-gallery').magnificPopup({
+            delegate: 'a',
+            type: 'image',
+            tLoading: 'Loading image #%curr%...',
+            mainClass: 'mfp-img-mobile',
+            gallery: {
+                enabled: true,
+                navigateByImgClick: true,
+                preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+            },
+            image: {
+                tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+            }
+        });
 
-})(jQuery); // End of use strict
+        //$("div.item p,h1").css({"font-size":"1.5em", "font-weight":"bold", "color":"#000"});
+
+
+
+
+    })(jQuery); // End of use strict
